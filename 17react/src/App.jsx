df78c7af038b31fff9a896845716fa9a7b3b4878
bfaatime <fatimebexseliyeva@icloud.com>
2024-12-17 17:06:30 +0400

    added

---
+++ 17react/src/App.jsx
@@ -0,0 +1,10 @@
+import ToDoList from './ToDoList.jsx'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <ToDoList/>
+  )
+}
+
+export default App
---
+++ 17react/src/ToDoList.jsx
@@ -0,0 +1,29 @@
+import React, { useState} from 'react'
+
+function ToDoList(){
+
+const [task, setTasks] = useState([]);
+const [newtask, setNewTask] = useState("");
+function handleInputChange(event) {
+    
+}
+function addTask () {
+    
+}
+
+function deleteTask(index) {
+    
+}
+
+function moveTaskUp(index) {
+    
+}
+
+function moveTaskDown (index) {
+    
+}
+
+return(<></>);
+
+}
+export default ToDoList
